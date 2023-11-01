f3ffb6888d43bfab1a73ea9b62ce1f6db5714985
Razer4m <tumelomorakile@gmail.com>
2023-11-01 03:43:10 +0200

    tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(9):
     for j in range(i + 1, 10):
         if i != j:
-            if i == 0:
-                print("{:d}{:d}, ".format(i, j), end="")
-            else:
-                print("{:02d}, ".format(i * 10 + j), end="")
+            print("{:02d}, ".format(i * 10 + j), end="")
 print("{:02d}".format(89))
