575429bdb580d38242f3c99176f43ad935ccca21
Razer4m <tumelomorakile@gmail.com>
2023-11-03 02:38:55 +0200

    Task 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main":
+if __name__ == "__main__":
     import hidden_4
 
     module_names = dir(hidden_4)
-    module_names.sort()
-
+  
     for name in module_names:
         if name[:2] != "__":
-                print(name)
+                print("{}".format(name))
