f92110fb54acbaa341c04564d8ecdc877e80ca50
Razer4m <tumelomorakile@gmail.com>
2023-11-07 01:07:19 +0200

    Task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = tuple_a[:2]
+    b = tuple_b[:2]
+
+    while len(a) < 2:
+        a += (0,)
+    while len(b) < 2:
+        b += (0,)
+
+    result = (a[0] + b[0], a[1] + b[1])
+    return result
