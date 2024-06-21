1b027b8b21e0773fb933a1c4a9b55654ed191aab
Razer4m <tumelomorakile@gmail.com>
2024-06-21 22:10:45 +0200

    Tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,11 @@
-#!/usr/bin/node
+#!/usr/bin/env node
+
 const args = process.argv.slice(2);
 
-if (process.argv.length === 0) {
-  console.log('No argument');
-} else if (process.argv.length === 1) {
-  console.log('Argument found');
+if (args.length === 0) {
+    console.log("No argument");
+} else if (args.length === 1) {
+    console.log("Argument found");
 } else {
-  console.log('Arguments found');
+    console.log("Arguments found");
 }
