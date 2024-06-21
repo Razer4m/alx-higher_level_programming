a2df48f97152c67c76f4f9e97d923b4660c18cb3
Razer4m <tumelomorakile@gmail.com>
2024-06-21 22:39:31 +0200

    taks

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+const a = parseInt(args[0]);
+const b = parseInt(args[1]);
+
+function add(a, b) {
+    return a + b;
+}
+
+if (isNaN(a) || isNaN(b)) {
+    console.log(NaN);
+} else {
+    console.log(add(a, b));
+}
