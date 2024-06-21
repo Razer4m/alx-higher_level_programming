f036311a071518a13913b3d72ff537d53ff4a0bc
Razer4m <tumelomorakile@gmail.com>
2024-06-21 22:04:34 +0200

    Tasks

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log('Not a number');
+} else {
+  const num = parseInt(args[0]);
+  if (isNaN(num)) {
+    console.log('Not a number');
+  } else {
+    console.log(`My number: ${num}`);
+  }
+}
