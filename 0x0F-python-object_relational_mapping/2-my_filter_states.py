d86b9d77af42178973fec8f3b1fabd9fc4765075
Razer4m <tumelomorakile@gmail.com>
2024-06-24 00:49:11 +0200

    Task1

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,31 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
 
 '''
-lists all states with starting name with N
-from the datab
+Script that lists all states from the database
 '''
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
-
-    
+    state_name = sys.argv[4]
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=mysql_username,
                          passwd=mysql_password,
                          db=database_name)
+
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    cursor.execute(query)
     rows = cursor.fetchall()
+
     for row in rows:
         print(row)
+
     cursor.close()
     db.close()
